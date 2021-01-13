20343cd5dba0ee9caaf5697991e9ec5be0205c3d
Scott McMahan <scottcmcmahan@gmail.com>
2021-01-13 12:53:42 -0600

    Fixed indent

--- ml_model.py
+++ ml_model.py
@@ -1,3 +1,5 @@
+
+
 class ML_model():
     from sklearn.ensemble import RandomForestRegressor
     from sklearn import linear_model
@@ -5,7 +7,8 @@ class ML_model():
     from sklearn.model_selection import train_test_split
     from sklearn.metrics import accuracy_score
     def __init__(self):
-    def make_predictions(self, X, model_list)
+        pass 
+    def make_predictions(self, X, model_list):
         X=self.X
         model_list=self.model_list
         
