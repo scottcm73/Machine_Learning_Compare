d2048301f2374dd6d5f530460105e16f98a4ae99
scottcm73 <scottcmcmahan@gmail.com>
2021-01-13 09:27:21 -0600

    Adding ML_model class

---
+++ ml_model.py
@@ -0,0 +1,10 @@
+class ML_model():
+    def __init__(self):
+    def make_predictions(self, X, model_list)
+        X=self.X
+        model_list=self.model_list
+        
+        return 
+    def crossvalidator(self):
+        return
+        
