227c064801aba8c47524a68cc755d7b315286be4
scottcm73 <scottcmcmahan@gmail.com>
2021-01-15 10:08:25 -0600

    Fleshed out cross_validator

--- ml_model.py
+++ ml_model.py
@@ -13,6 +13,21 @@ class ML_model():
         model_list=self.model_list
         
         return 
-    def crossvalidator(self):
+    def crossvalidator(self, y_true):
+        self.model_list=model_list
+        result_ave_list=[]
+        for ml_num in range(0, model_list):
+            y=model[]()
+
+            result=crossvalidate(model[],X,y)
+            result_ave=(sum(result))/(len(result))
+            result_ave_list.append(result_ave)
+            self.result_ave_list=result_ave_list
+
+        return result_ave_list
+    
+    def do_accuracy_score(self, y_true, y_pred):
+
+        score=[]
+        acuracy_score(y_true, y_pred)
         return
-        
