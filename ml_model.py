077cf83fc9fff89cb07b2b7a7767fd73768428b8
scottcm73 <scottcmcmahan@gmail.com>
2021-01-23 20:22:30 -0600

    Fixed object instantiation, column drop statement

--- ml_model.py
+++ ml_model.py
@@ -1,37 +1,4 @@
 
-
-class ML_model_compare():
-
-    
-    def __init__(self):
-        pass 
-    #make_predictions function assumes all numbers in data_df dataframe. This may mean converting datetimes to number columns
-    def make_predictions(self, model_dict, data_df):
-        
-        X=self.X
-        model_dict=self.model_dict
-        
-
-        y_pred=0
-
-        
-        return y_pred
-    def crossvalidator(self, y_true):
-        self.model_dict=model_dict
-        result_ave_list=[]
-
-
-    
-
-        return  
-    
-    def do_accuracy_score(self, y_true, y_pred):
-
-        score=[]
-        acuracy_score(y_true, y_pred)
-        return
-
-
 ### Take in data for model1 ###
 from sklearn.ensemble import RandomForestRegressor
 from sklearn import linear_model
@@ -46,6 +13,62 @@ from sklearn.ensemble import GradientBoostingRegressor
 import pandas as pd
 import os
 import datetime
+class ML_model_compare:
+
+    
+    def __init__(self, model_dict, data_df):
+        # self.model_dict=model_dict
+        self.data_df=data_df
+        self.model_dict=model_dict
+        print(data_df)
+ 
+    #make_predictions function assumes all numbers in data_df dataframe. This may mean converting datetimes to number columns
+    
+    def traintestsplit(self):
+        this_df=self.data_df
+ 
+       
+        X =self.data_df[["Open", "Low", "Close", "Adj Close", "Volume"]]
+        y = self.data_df[["High"]]
+        model_dict = self.model_dict
+        X_train, X_test, y_train, y_test = train_test_split(
+            X, y, test_size=0.33, random_state=42)
+        self.X_train=X_train
+        self.X_test=X_test
+        self.y_train=y_train
+        self.y_test=y_test
+    def feature_selection(self):
+        return
+  
+    
+    def make_predictions(self):
+        
+        
+       
+        
+
+        y_pred=0
+
+        
+        return y_pred
+
+
+    def crossvalidator(self):
+
+
+
+    
+
+        return  
+    
+    def do_accuracy_score(self):
+
+        score=[]
+        acuracy_score(y_true, y_pred)
+        return
+
+
+
 
 def getdata():
     the_file="EOS-USD.csv"
@@ -86,19 +109,19 @@ print(data_df.info())
 # Work with datetime later
 # For now remove datetime column 
 
-data_df = data_df.drop("Date", 1, inplace=True)
-# the line above will eventuall be deleted.
+data_df.drop(["Date"], axis=1, inplace=True)
+# The line above will eventually be deleted.
 
 
 
 model_dict={"LR":linearreg(), "MLR": multiLR(), "RFReg": randFR(),}
 
 
-key_list=model_dict.keys()
-print(key_list)
+# key_list=model_dict.keys()
+# print(key_list)
 
-for name, model in model_dict.items():
-    name_model = model
+# for name, model in model_dict.items():
+#     name_model = model
     # name_fit = name_model.fit(X_train, y_train)
     # name_pred = name_model.predict(X_test)
     # name_train_score = name_model.score(X_train, y_train).round(4)
@@ -108,4 +131,17 @@ for name, model in model_dict.items():
     # test_scores.append(name_test_score)
 
 # score_df = pd.DataFrame(names, train_scores, test_scores)
-ml_compare=ML_model_compare
+print(data_df.head())
+ml_compare=ML_model_compare(model_dict, data_df)
+
+this_df=ml_compare.data_df
+print(this_df)
+
+ml_compare.traintestsplit()
+
+X_train = ml_compare.X_train
+y_train = ml_compare.y_train
+X_test = ml_compare.X_test
+y_test = ml_compare.y_test
+
+print(y_test.head())
